ab4f1c7ee91b3a6e6602531abfea79e1a0ed7d11
Jeff Borowitz <jborowitz@gmail.com>
2013-05-30 14:07:23 +0000

    Got data frame together
    I have created the little data frame that I need to plot: it has points at
    the lat/long centers of census tracts or other geographical areas

--- map.r
+++ map.r
@@ -3,7 +3,7 @@ library(ggplot2)
 library(UScensus2010)
 #ggplot(subset(chi,Primary.Type == "NARCOTICS"), aes(Longitude, Latitude)) + stat_density2d(aes(fill=..level..), geom="polygon") + scale_alpha_continuous(limits=c(0,0.2),breaks=seq(0,0.2,by=0.025))+
 #chi<-read.csv('/home/ubuntu/maps/chicago.csv')
-load('chicago.rdata')
+load('/mnt/maps/chicago.rdata')
 lonrange<-max(chi$Longitude,na.rm=TRUE)-min(chi$Longitude,na.rm=TRUE)
 latrange<-max(chi$Latitude,na.rm=TRUE)-min(chi$Latitude,na.rm=TRUE)
 extra<-.2
@@ -17,12 +17,20 @@ p <- p + stat_density2d(data=chi,aes(x=Longitude, y=Latitude, fill=..level..),ge
 # Note: with the full chicago data set, it looks like I need about 8GB of memory
 p
 # UScensus2010::install.tract(x='linux')
-b<-county(name='cook',state='illinois',level='blkgrp')
+b<-county(name='cook',state='illinois',level='tract')
 d<-fortify(b)
-e<-data.frame("id"=rownames(b@data),"P0010001"=b$P0010001)
+e<-data.frame("id"=rownames(b@data),"P0010001"=b$P0010001,"black"=b$P0030003,"white"=b$P0030002)
 # There must be a better way to do this, but I haven't done it yet.  I am fortifying d to make the plottable
 # object, but then I am also creating a new dataframe with the 'id' variable to merge in data.  
 # But it does work
+locations<-ldply(b@polygons,.fun=function(x){
+  ll<-x@labpt
+  long<-ll[1]
+  lat<-ll[2]
+  id<-x@ID
+  return(data.frame("id"=id, "long"=long, "lat"=lat))
+})
+e<-merge(e,locations)
 f<-merge(d,e,by="id")
 f$popfact<-cut(f$P0010001,breaks=summary(f$P0010001))
 ggplot(f,aes(x=long,y=lat, group=group,fill=popfact)) + geom_polygon() + geom_path(color="white") + coord_equal() + scale_fill_brewer("Population or something?")
@@ -36,7 +44,14 @@ ggplot(f,aes(x=long,y=lat, group=group,fill=P0010001)) + geom_polygon() + geom_p
 # polygons, or else a simplification which would work well, like putting
 # all the people at the center of the tracts and then doing a kernel
 # density
+# b@polygons[[1]]@labpt-> this will access the lat/long point of a given location
 
 # Note: the plots above are a little useless.  What I really need is a 
 # dataset that is not at the point level to draw block groups but at the 
 # lat-lon center level, where each value is assigned to the center of the block group
+p<-ggmap(m)
+p<-p+ geom_point(data=e,aes(x=long, y=lat, size=black),colour='green',alpha=.2) + geom_point(data=e,aes(x=long, y=lat, size=white),colour='red',alpha=.2)
+# ^ This code plots the black/white stuff on top of the google map
+# ggplot(e,aes(x=long,y=lat)) + geom_point(aes(size=black),colour='blue',alpha=.4) + geom_point(aes(size=white),colour='red',alpha=.4)
+#^ plot the black/white data
+p <- p + stat_density2d(data=subset(chi,Primary.Type == "NARCOTICS"),aes(x=Longitude, y=Latitude, fill=..level..),geom='polygon',alpha=.4)
