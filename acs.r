1ac7cf81375ce056b5f9fd6f1b4afeaabde730ba
Jeff Borowitz <jborowitz@gmail.com>
2014-09-19 15:47:16 +0000

    Add header, some data cleaning

--- acs.r
+++ acs.r
@@ -4,3 +4,11 @@ cook <- geo.make(county='Cook', state='IL')
 output <- acs.fetch(geography=cook, table.number="B01001")
 geo.lookup(place='Brooklyn') # Search for multiple geographical results
 
+reg$US <- grepl(" USA", reg$location)
+# Determine which addresses are in the US
+reg <- reg[reg$US,] # Keep only the american addresses
+reg$location<-gsub(", USA","",reg$location)  # Remove the USA piece
+reg$state<-gsub('.*, ','', reg$location, perl=T) 
+reg$city<-gsub(', .*','', reg$location, perl=T)
+# put city and state into columns, based on what's before or after the ', '
+# delimiter
