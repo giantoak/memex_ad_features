93d8aba3a70cd73efaac758ec234bec39cfba58c
Jeff Borowitz <jborowitz@gmail.com>
2014-09-25 01:18:33 +0000

    Merge in unemployment at state level

--- costs.r
+++ costs.r
@@ -38,3 +38,24 @@ sums<-ddply(a, c("state"), function(df)c(mean(df$Cost_hour_mean),
 names(sums) <- c("state","avg_cost", "sd_cost", "se_cost", "avg_age",
                  "sd_age", "se_age", "n")
 
+b <- read.csv('frac_with_price.csv', header=FALSE, col.names=c('state', 'frac'))
+d<-merge(sums, b)
+# Merge in the fraction data
+
+states<-read.csv('states.txt', stringsAsFactors=FALSE)
+series<-paste("FRBC/UNEMP_ST_", states$abbrev, sep='') # create unemp series names
+library(Quandl)
+unemp<-Quandl(series, trim <- start="2013-01-01", trim <- end="2013-12-01") 
+# read unemployment data from Quandl
+avg.unemp<-sapply(unemp, FUN=mean,MARGIN=2) # take annual unemployment averages
+names(avg.unemp)<-gsub(' - Value','',gsub('FRBC.UNEMP_ST_','',names(avg.unemp)))
+# replace the names with column names that will merge in
+avg.unemp<-avg.unemp[2:51] # remove PR and the date column
+
+d$unemp<-NA
+for (i in names(avg.unemp)){
+    print(i)
+    print(sum(d$state == i))
+    state.name <- states[states$abbrev == i,'state']
+    d$unemp[as.character(d$state) == state.name] <- avg.unemp[i]
+}
