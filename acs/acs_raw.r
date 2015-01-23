b5918fe4f09fa0820fec57a6d0a2dd513216f7f0
Ubuntu <ubuntu@ip-10-30-216-79.ec2.internal>
2015-01-23 16:39:40 +0000

    Working version of acs tabulations

--- acs/acs_raw.r
+++ acs/acs_raw.r
@@ -1,12 +1,8 @@
 library(plyr)
 library(Hmisc)
-#a<-read.csv('/mnt/mount/acsmicro/all.csv')
-a<-read.csv('/mnt/mount/acsmicro/test.csv')
-#a1<-read.csv('/mnt/mount/acsmicro/ss13pusa.csv')
-#a2<-read.csv('/mnt/mount/acsmicro/ss13pusb.csv')
-#a3<-read.csv('/mnt/mount/acsmicro/ss13pusc.csv')
-#a4<-read.csv('/mnt/mount/acsmicro/ss13pusd.csv')
-#a<-rbind(a1, a2, a3, a4)
+a<-read.csv('/mnt/mount/acsmicro/test.csv') # This csv is just a few lines
+#of text, which we use to figure out column names to avoid loading unused
+#columns
 keep_cols<-c("PUMA00", "PUMA10", "ST",
              "WKHP","WKW","AGEP","JWMNP", "WAGP", "ADJINC", "ESR",
              "INDP", "NAICSP", "OCCP02", "OCCP10", "OCCP12", "PERNP",
@@ -19,18 +15,8 @@ a<-read.csv('/mnt/mount/acsmicro/all.csv', colClasses=col.classes)
 cat('Data loaded!!!\n')
 
 a$PUMA<- a$PUMA00
-a$PUMA[a$PUMA < 0] <-a$PUMA10
+a$PUMA[a$PUMA < 0] <-a$PUMA10[a$PUMA < 0]
 # Use either the 2000 or 2010 pumas TODO: fix this, to use a true crosswalk
-
-#buffersize = 2000 )
-
-#a$PWGTP<-a$PWGTP / 100
-a$strata <- 100000*a$ST + a$PUMA  
-#a<-a[a$incwage > 0,] # Restrict to only people with positive wage earnings
-## Note: incwage is in dollars
-#a<-a[a$uhrswork > 30,] # Restrict to only full time workers
-#cat('subsetting done!\n')
-# a<-a[a$wkswork2 >= 5,] # Restrict to year round workers
 
 #Key Variables:
 #AGEP: age, 1-99, topcoded
@@ -52,6 +38,8 @@ a$strata <- 100000*a$ST + a$PUMA
 #SOCP00: 2009 occupation based on 2000 SOC
 #SOCP10: 2010/2011 occupation based on 2010 SOC
 #SOCP12: 2012 occupation based on 2010 SOC
+# For others see the codebook!
+# http://www.census.gov/acs/www/Downloads/data_documentation/pums/DataDict/PUMS_Data_Dictionary_2009-2013.pdf
 
 a<-a[!is.na(a$SOCP00),] # Drop missing occupations
 a<-a[!is.na(a$WKHP),]# Drop People with missing hours
@@ -64,8 +52,8 @@ a$ADJINC<-a$ADJINC/1e6 # This is a translation factor to weight dollars in 2013 
 a$wage <- a$WAGP / (a$WKHP * 50) * a$ADJINC
 
 a$SOC <- as.character(a$SOCP00)
-a$SOC[a$SOC=="N.A."]<-as.character(a$SOCP10[a$SOC=="N.A."]<-a$SOCP10)
-a$SOC[a$SOC=="N.A."]<-as.character(a$SOCP12[a$SOC=="N.A."]<-a$SOCP12)
+a$SOC[a$SOC=="N.A."]<-as.character(a$SOCP10[a$SOC=="N.A."])
+a$SOC[a$SOC=="N.A."]<-as.character(a$SOCP12[a$SOC=="N.A."])
 # Recode all occupations to 2000 levels TODO: fix this!
 
 a$OCC <- a$OCCP02
@@ -98,29 +86,9 @@ computes<-function(x){
     x$N<-dim(x)[1]
     x$sum.wght<-sum(x$PWGTP)
     #print(x[1,names])
-    return(x[1,c('OCC2','SOC','PUMA','ST',names)])
+    return(x[1,c('OCC2','SOC2','PUMA','ST',names)])
 }
 
 cat('doing ddply\n')
 b<-ddply(.data=a, .variables=.(OCC2, SOC2, PUMA, ST), .fun=computes)
-#write.csv(b, file='wage_bins.csv', row.names=FALSE)
-
-#cat('creating counts\n')
-#counts<-as.data.frame(table(a$naics2, a$occ2, a$puma, a$statefip))
-#names(counts)<-c('naics2','occ2','puma','statefip','Freq')
-#a<-merge(a,counts)
-##print(dim(a))
-##a<-a[a$Freq > 10,]
-##print(dim(a))
-#cat('loading survey design\n')
-#ipums.design <- svydesign(id=~a$serial, strata=~a$strata, data=a, weights=a$PWGTP)
-#cat('survey design compelted...\n')
-
-##b<-svytable(incwage ~ occsoc + indnaics + statefip + puma, ipums.design) 
-#b<-svyby(~incwage, ~naics2 + occ2 + puma + statefip, ipums.design, svymean) 
-#cat('survey by compelted...\n')
-#write.csv(b, file='wage_means.csv', row.names=FALSE)
-
-#d<-svyby(~incwage, ~naics2 + occ2 + puma + statefip, ipums.design, svyquantile, quantiles=c(.1,.9))
-## This command would do .1 and .9 quantiles, but appears to choke on empty
-## cells
+write.csv(b, file='wage_bins_raw.csv', row.names=FALSE)
