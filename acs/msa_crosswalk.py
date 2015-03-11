1ef52a79652c70b6799bdec5a4590d411ac21cba
Jeffrey Borowitz <jeffrey.borowitz@giantoak.com>
2015-03-11 17:02:59 -0400

    Finished most of MSA crosswalk

--- acs/msa_crosswalk.py
+++ acs/msa_crosswalk.py
@@ -43,6 +43,47 @@ out.loc[out.ipums_code==348,'qcew_code'] = 'C2690' # Indianapolis, IN
 out.loc[out.ipums_code==404,'qcew_code'] = 'C2962' # Lansing, MI
 out.loc[out.ipums_code==412,'qcew_code'] = 'C2982' # Las Vegas, NV
 out.loc[out.ipums_code==440,'qcew_code'] = 'C3078' # Little Rock, AK
+out.loc[out.ipums_code==440,'qcew_code'] = 'C3078' # Little Rock, AK
+out.loc[out.ipums_code==448,'qcew_code'] = 'C3108' # Los Angeles, CA (Note: there are 2 LA MSAs, one with Anaheim, one with Santa Ana
+out.loc[out.ipums_code==452,'qcew_code'] = 'C3114' # Louisville, KY
+out.loc[out.ipums_code==476,'qcew_code'] = 'C3170' # Manchester, NH
+out.loc[out.ipums_code==490,'qcew_code'] = 'C3734' # Melbourne, FL
+out.loc[out.ipums_code==492,'qcew_code'] = 'C3282' # Memphis, TN
+out.loc[out.ipums_code==500,'qcew_code'] = 'C3310' # Miami, FL (Again)
+out.loc[out.ipums_code==508,'qcew_code'] = 'C3334' # Milwaukee, WI
+out.loc[out.ipums_code==512,'qcew_code'] = 'C3346' # Minneapolis, MN
+out.loc[out.ipums_code==534,'qcew_code'] = 'C3498' # Nashville, TN
+out.loc[out.ipums_code==556,'qcew_code'] = 'C3538' # New Orleans, LA
+out.loc[out.ipums_code==560,'qcew_code'] = 'C3562' # New York, NY
+out.loc[out.ipums_code==572,'qcew_code'] = 'C4726' # Norfolk, VA
+out.loc[out.ipums_code==592,'qcew_code'] = 'C3654' # Omaha, NE
+out.loc[out.ipums_code==596,'qcew_code'] = 'C3674' # Orlando, FL
+out.loc[out.ipums_code==608,'qcew_code'] = 'C3786' # Pensacola, FL
+out.loc[out.ipums_code==616,'qcew_code'] = 'C3798' # Philadelphia, PA
+out.loc[out.ipums_code==620,'qcew_code'] = 'C3806' # Phoenix, AZ
+out.loc[out.ipums_code==640,'qcew_code'] = 'C3886' # Portland, ME
+out.loc[out.ipums_code==644,'qcew_code'] = 'C3890' # Portland, OR
+out.loc[out.ipums_code==648,'qcew_code'] = 'C3930' # Providence, RI
+out.loc[out.ipums_code==664,'qcew_code'] = 'C3958' # Raleigh, NC
+out.loc[out.ipums_code==676,'qcew_code'] = 'C4006' # Richmond, VA
+out.loc[out.ipums_code==678,'qcew_code'] = 'C4014' # Riverside, CA
+out.loc[out.ipums_code==692,'qcew_code'] = 'C4090' # Sacramento, CA
+out.loc[out.ipums_code==716,'qcew_code'] = 'C4162' # Salt Lake City, UT
+out.loc[out.ipums_code==724,'qcew_code'] = 'C4170' # San Antonio, TX
+out.loc[out.ipums_code==732,'qcew_code'] = 'C4174' # San Diego, TX
+out.loc[out.ipums_code==736,'qcew_code'] = 'C4186' # San Francisco, CA
+out.loc[out.ipums_code==740,'qcew_code'] = 'C4194' # San Jose, CA
+out.loc[out.ipums_code==747,'qcew_code'] = 'C4206' # Santa Barbara, CA
+out.loc[out.ipums_code==748,'qcew_code'] = 'C4210' # Santa Cruz, CA
+out.loc[out.ipums_code==751,'qcew_code'] = 'C4226' # Santa Cruz, CA
+out.loc[out.ipums_code==756,'qcew_code'] = 'C4254' # Scranton, PA
+out.loc[out.ipums_code==760,'qcew_code'] = 'C4266' # Seattle, WA
+out.loc[out.ipums_code==784,'qcew_code'] = 'C4406' # Spokane, WA
+out.loc[out.ipums_code==812,'qcew_code'] = 'C4470' # Stockton, CA
+out.loc[out.ipums_code==820,'qcew_code'] = 'C4266' # Seattle, WA (Again)
+out.loc[out.ipums_code==884,'qcew_code'] = 'C4790' # Washington, DC
+out.loc[out.ipums_code==896,'qcew_code'] = 'C3310' # West Palm Beach, FL (Again)
+out.loc[out.ipums_code==916,'qcew_code'] = 'C3798' # Wilmington, DE (now Philadelphia)
 
 out.loc[out.qcew_code.isnull(),'ipums_code']
 out[out.ipums_code.isnull()]
