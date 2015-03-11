ff3dc60d0abefde00632b52aaabbada499eda53f
Jeffrey Borowitz <jeffrey.borowitz@giantoak.com>
2015-03-11 19:17:04 +0000

    Working on munging qcew

--- acs/qcew.py
+++ acs/qcew.py
@@ -10,3 +10,8 @@ for i in glob.glob('*singlefile*csv'):
 #a=pandas.read_csv("2014.q1-q2.singlefile.csv")
 #a=a[a.agglvl_code == 44]   # Select only major industry
 data = pandas.concat(outputs, axis=0)
+e=pandas.melt(d,id_vars=[u'area_fips', u'industry_code', u'year', u'qtr'], var_name='sub_month', value_name='employment') # Reshape to make months verticle instead of horizontal... 
+e['sub_month'].replace({'month1_emplvl':1,'month2_emplvl':2,'month3_emplvl':3}, inplace=True) # Remap month names
+e['month'] = (e['qtr'] - 1) * 3 + e['sub_month'] # Create month of year var
+del e['sub_month']
+del e['qtr']
