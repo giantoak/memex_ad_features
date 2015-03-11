8e9f8da31acbd32b65ea4ab42a0ecc04cc9638ca
Jeffrey Borowitz <jeffrey.borowitz@giantoak.com>
2015-03-11 10:51:41 -0400

    updating census code

--- acs/census_2000.r
+++ acs/census_2000.r
@@ -19,6 +19,7 @@
   #UHRSWORK           P  72-73           2      X
   #INCWAGE            P  74-79           6      X
 library(plyr)
+library(HMisc)
 cn<-c("year","datanum","serial","hhwt","region","stateicp","statefip","county","city","puma","gq","pernum","perwt",
       "sex","age","indnaics","wkswork2","uhrswork","incwage")
       #"occ","ind","occsoc","indnaics","uhrswork","incwage")
@@ -53,22 +54,22 @@ cat('data read to mem\n')
 #print('data loaded!!!')
 ##buffersize = 2000 )
 
-#a$hhwt<-a$hhwt / 100
-#a$perwt<-a$perwt / 100
-#a$strata <- 100000*a$statefip + a$puma  
-#a<-a[a$incwage > 0,] # Restrict to only people with positive wage earnings
-## Note: incwage is in dollars
-#a<-a[a$uhrswork > 30,] # Restrict to only full time workers
-#cat('subsetting done!\n')
-## a<-a[a$wkswork2 >= 5,] # Restrict to year round workers
+a$hhwt<-a$hhwt / 100
+a$perwt<-a$perwt / 100
+a$strata <- 100000*a$statefip + a$puma  
+a<-a[a$incwage > 0,] # Restrict to only people with positive wage earnings
+# Note: incwage is in dollars
+a<-a[a$uhrswork > 30,] # Restrict to only full time workers
+cat('subsetting done!\n')
+# a<-a[a$wkswork2 >= 5,] # Restrict to year round workers
 
-#a$incwage<-a$incwage / 2000 # A stand-in for doing the FT/year round selection
+a$incwage<-a$incwage / 2000 # A stand-in for doing the FT/year round selection
 
-#a$naicschars<-as.character(a$indnaics)
-#a$naics2<-as.factor(unlist(lapply(a$naicschars, FUN=function(x){return(substring(x,1,2))}))) # get 2 digit industries)
-## Can recode these back and forth using the IPUMS industry crosswalk here:
-## https://usa.ipums.org/usa/volii/indcross03.shtml
-#a$naicschars<-NULL
+a$naicschars<-as.character(a$indnaics)
+a$naics2<-as.factor(unlist(lapply(a$naicschars, FUN=function(x){return(substring(x,1,2))}))) # get 2 digit industries)
+# Can recode these back and forth using the IPUMS industry crosswalk here:
+# https://usa.ipums.org/usa/volii/indcross03.shtml
+a$naicschars<-NULL
 
 #a$occchars<-as.character(a$occsoc)
 #a$occ2<-as.factor(unlist(lapply(a$occchars, FUN=function(x){return(substring(x,1,2))}))) # get 2 digit industries)
@@ -77,32 +78,32 @@ cat('data read to mem\n')
 #a$occchars<-NULL
 #require(survey)
 
-#names<-c('mean.wage','var.wage','p05','p10','p25','p50','p75','p90','p95','N','sum.wght')
-#computes<-function(x){
-    #x$mean.wage<-wtd.mean(x$incwage, weights=x$perwt)
-    #x$var.wage<-wtd.var(x$incwage, weights=x$perwt)
-    #quantiles<-c(.05,.1, .25, .5, .75, .9,.95)
-    #quantile.results<-wtd.quantile(x$incwage, weights=x$perwt, probs=quantiles)
-    #x$p05<-quantile.results[1]
-    #x$p10<-quantile.results[2]
-    #x$p25<-quantile.results[3]
-    #x$p50<-quantile.results[4]
-    #x$p75<-quantile.results[5]
-    #x$p90<-quantile.results[6]
-    #x$p95<-quantile.results[7]
-    #x$N<-dim(x)[1]
-    #x$sum.wght<-sum(x$perwt)
-    ##print(x[1,names])
-    #return(x[1,c('occ2','naics2','puma','statefip',names)])
-#}
+names<-c('mean.wage','var.wage','p05','p10','p25','p50','p75','p90','p95','N','sum.wght')
+computes<-function(x){
+    x$mean.wage<-wtd.mean(x$incwage, weights=x$perwt)
+    x$var.wage<-wtd.var(x$incwage, weights=x$perwt)
+    quantiles<-c(.05,.1, .25, .5, .75, .9,.95)
+    quantile.results<-wtd.quantile(x$incwage, weights=x$perwt, probs=quantiles)
+    x$p05<-quantile.results[1]
+    x$p10<-quantile.results[2]
+    x$p25<-quantile.results[3]
+    x$p50<-quantile.results[4]
+    x$p75<-quantile.results[5]
+    x$p90<-quantile.results[6]
+    x$p95<-quantile.results[7]
+    x$N<-dim(x)[1]
+    x$sum.wght<-sum(x$perwt)
+    #print(x[1,names])
+    return(x[1,c('sex','naics2','puma','statefip',names)])
+}
 
-#cat('doing ddply\n')
-#b<-ddply(.data=a, .variables=.(occ2, naics2, puma, statefip), .fun=computes)
-#write.csv(b, file='wage_bins.csv', row.names=FALSE)
+cat('doing ddply\n')
+b<-ddply(.data=a, .variables=.(naics2, sex, puma, statefip), .fun=computes)
+write.csv(b, file='wage_bins.csv', row.names=FALSE)
 
 #cat('creating counts\n')
-#counts<-as.data.frame(table(a$naics2, a$occ2, a$puma, a$statefip))
-#names(counts)<-c('naics2','occ2','puma','statefip','Freq')
+#counts<-as.data.frame(table(a$naics2, a$puma, a$statefip))
+#names(counts)<-c('naics2','puma','statefip','Freq')
 #a<-merge(a,counts)
 ##print(dim(a))
 ##a<-a[a$Freq > 10,]
@@ -112,10 +113,10 @@ cat('data read to mem\n')
 #cat('survey design compelted...\n')
 
 ##b<-svytable(incwage ~ occsoc + indnaics + statefip + puma, ipums.design) 
-#b<-svyby(~incwage, ~naics2 + occ2 + puma + statefip, ipums.design, svymean) 
+#b<-svyby(~incwage, ~naics2 + sex + puma + statefip, ipums.design, svymean) 
 #cat('survey by compelted...\n')
 #write.csv(b, file='wage_means.csv', row.names=FALSE)
 
-#d<-svyby(~incwage, ~naics2 + occ2 + puma + statefip, ipums.design, svyquantile, quantiles=c(.1,.9))
+#d<-svyby(~incwage, ~naics2 + sex + puma + statefip, ipums.design, svyquantile, quantiles=c(.1,.9))
 ## This command would do .1 and .9 quantiles, but appears to choke on empty
 ## cells
