df17b4737f45662f42d6033bc0c38e9b73fb0ee2
Jeffrey Borowitz <jeffrey.borowitz@giantoak.com>
2015-03-17 23:49:45 -0500

    Keep merge variable

--- nibrs/pd.py
+++ nibrs/pd.py
@@ -1,4 +1,5 @@
 import numpy
+import ipdb
 import pandas
 locations=((0,2),
 (2,4),
@@ -901,8 +902,9 @@ cross_walk = pandas.read_csv('crosswalk_tract_msa.csv')
 #FIPS State Code (FSTATE)
 #FIPS County Code (FCOUNTY)
 #FIPS Place code (FPLACE)
-cross_walk = cross_walk[['ORI9', 'PLACENM','AGENCY','FSTATE','FCOUNTY','FPLACE', 'UPOPTOT','MSA','msaname','countyno']]
+cross_walk = cross_walk[['ORI9', 'PLACENM','AGENCY','FSTATE','FCOUNTY','FPLACE', 'UPOPTOT','MSA','msaname','countyno','_merge']]
 cross_walk = cross_walk[~cross_walk.MSA.isnull()]
+cross_walk = cross_walk[cross_walk['_merge'] == 'matched (3)']
 cross_walk['codes'] = cross_walk.MSA.apply(lambda x: '31000US%s' % str(int(x)))
 m = pandas.merge( m, cross_walk[['ORI9','codes']], left_on='ORI', right_on='ORI9') # Merge acts onto MSAs by ORI code
 b=m.groupby('codes')[['prostitution','female_violence','violence']].aggregate([numpy.size, numpy.mean, numpy.sum])
