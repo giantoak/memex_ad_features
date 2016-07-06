7b6900537b23971c205a98829fecd123f86d5cce
pmlandwehr <pmlandwehr@gmail.com>
2016-07-06 17:36:36 -0400

    Updated makefile graph generator and graph

--- make_makefile_graph.py
+++ make_makefile_graph.py
@@ -12,17 +12,50 @@ color_dict = {
 }
 
 
+def dict_to_graph_var(var_name, feature_dict):
+    """
+
+    :param str var_name:
+    :param dict feature_dict:
+    :return: `str` --
+    """
+    return '{} [{}];'.format(var_name,
+                             ', '.join(['{}="{}"'.format(f, feature_dict[f])
+                                        for f in feature_dict]))
+
+
+def list_to_cluster(node_list, rank=None):
+    """
+
+    :param list node_list:
+    :param str rank:
+    :return: `str` --
+    """
+    cluster_str = ', '.join(node_list)
+    if rank is None:
+        return '{ ' + cluster_str + ' }'
+    return '{ rank="' + rank + '" ' + cluster_str + ' }'
+
+
 def main():
     lines = open('Makefile', 'r').readlines()
     target_lines = [x for x in lines if x[0] != '\t' and x.find(':') > 0]
 
-    edge_list = []
+    graph_features = {'label': 'Makefile Target Order',
+                      'rankdir': 'LR',
+                      'layout': 'dot',
+                      'concentrate': 'true'}
+    node_features = {'style': 'filled'}
+
+    orphan_nodes = set()
+    childless_nodes = set()
 
     node_dict = {}
     node_color_dict = {}
-    pos_dict = {}
+    target_source_dict = {}
 
     node_count = 0
+    cluster_count = 0
 
     for line in target_lines:
         target, sources = line.strip().split(':')
@@ -30,42 +63,60 @@ def main():
 
         for node in [target] + source_list:
             if node not in node_dict:
-                node_name = 'node_{}'.format(node_count)
-                node_dict[node] = node_name
-                node_count += 1
                 suffix = node.split('.')[-1]
                 if suffix in color_dict:
+                    node_name = 'node_{}'.format(node_count)
+                    node_dict[node] = node_name
+                    node_count += 1
                     node_color_dict[node] = color_dict[suffix]
+
                 else:
+                    node_name = 'cluster_{}'.format(cluster_count)
+                    node_dict[node] = node_name
+                    cluster_count += 1
                     node_color_dict[node] = color_dict['']
 
-        max_prev = max([0] + [pos_dict[source] for source in source_list if source in pos_dict])
-        if target in pos_dict:
-            pos_dict[target] = max(max_prev + 1, pos_dict[target])
+                if node == target:
+                    childless_nodes.add(node_dict[node])
+
+        if len(source_list) > 0:
+            node_set = set(node_dict[src] for src in source_list)
+            target_source_dict[node_dict[target]] = node_set
+            childless_nodes.difference_update(node_set)
+
         else:
-            pos_dict[target] = max_prev + 1
+            orphan_nodes.add(node_dict[target])
 
-        for source in source_list:
+    # Being childless is more important than being an orphan
+    orphan_nodes.difference_update(childless_nodes)
 
-            edge_list.append('{} -> {}'.format(node_dict[source], node_dict[target]))
 
-            if source in pos_dict:
-                continue
-
-            pos_dict[source] = pos_dict[target] - 1
 
     with open('makefile_graph.dot', 'w') as outfile:
         outfile.write('digraph Make_Targets {\n')
-        outfile.write('graph [label = "Makefile Target Order", rankdir="LR", layout="dot"];\n')
-        outfile.write('node [style=filled];\n')
+        outfile.write('  {}\n'.format(dict_to_graph_var('graph', graph_features)))
+        outfile.write('  {}\n'.format(dict_to_graph_var('node', node_features)))
         for node in node_dict:
-            outfile.write('  {} [label="{}", fillcolor="{}", rank="{}"];\n'.format(node_dict[node],
-                                                                                   node,
-                                                                                   node_color_dict[node],
-                                                                                   pos_dict[node]))
+            outfile.write('  {} [label="{}", fillcolor="{}"];\n'.format(node_dict[node],
+                                                                        node,
+                                                                        node_color_dict[node]))
 
-        for line in edge_list:
-            outfile.write('  {};\n'.format(line))
+        # outfile.write('  {};\n'.format(list_to_cluster(list(orphan_nodes), 'source')))
+        outfile.write('  {};\n'.format(list_to_cluster(list(childless_nodes), 'sink')))
+
+        # for target in target_source_dict:
+        #     shared_level = list(target_source_dict[target] - orphan_nodes)
+        #     if len(shared_level) > 1:
+        #         outfile.write('  {};\n'.format(list_to_cluster(shared_level, 'same')))
+
+        for target in target_source_dict:
+            if len(target_source_dict[target]) > 1:
+                outfile.write('  {} -> {};\n'.format(
+                    list_to_cluster(target_source_dict[target].difference(orphan_nodes)), target))
+            else:
+                outfile.write('  {} -> {};\n'.format(list(target_source_dict[target])[0],
+                                                     target))
+
         outfile.write('}\n')
 
 
