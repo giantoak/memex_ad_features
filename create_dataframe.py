66703b03daf1f379a0a0cfab3acd0f616f84d5ab
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-07-29 19:08:49 -0400

    Switch to bulk reading of tables

--- create_dataframe.py
+++ create_dataframe.py
@@ -1,19 +1,55 @@
-import pandas as pd
-import ujson as json
-import gzip
-
-
-def _df_from_flattened_json(config, columns=None):
+def gzipped_jsonline_file_to_df(fpath):
     """
 
-    :param dict config:
-    :param str | list columns: columns to use
-    :returns: `pd.DataFrame` --
+    :param str fpath: Path to GZIP file of JSON lines
     """
-    return pd.concat([pd.DataFrame.from_records([json.loads(line)
-                                                 for line in gzip.open(filename)],
-                                                columns=columns)
-                      for filename in config['filenames'][0:2]])
+    import ujson as json
+    from pandas import DataFrame as DF
+    import gzip
+    from numpy import nan
+
+    cols_to_use = ['_id', 'age',
+                   'email', 'flag',
+                   'location_city_lat_lon', 'location_city_name',
+                   'location_state_lat_lon', 'location_state_name',
+                   'phone', 'postdatetime',
+                   'rate']
+
+    df = DF.from_records((json.loads(line) for line in gzip.open(fpath)),
+                         columns=cols_to_use)
+    for loc_type in ['city', 'state']:
+        col = 'location_{}_lat_lon'.format(loc_type)
+        df.loc[:, col] = df.loc[:, col].apply(lambda x:
+                                              (x[0], x[1]) if isinstance(x, list)
+                                              else (nan, nan))
+
+    return df.drop_duplicates()
+
+
+def bulk_gzipped_jsonline_files_to_df(glob_or_list, nproc=20):
+    """
+    This returns a DataFrame of the content of glob_or_list. IT MAY CONTAIN DUPLICATES
+    :param str glob_or_list:
+    :param int nproc:
+    :returns: `pandas.DataFrame` --
+    """
+    import multiprocessing as mp
+    from tqdm import tqdm
+    import pandas as pd
+
+    if isinstance(glob_or_list, str):
+        from glob import glob
+        glob_or_list = glob(glob_or_list)
+
+    pool = mp.Pool(min(nproc, len(glob_or_list)))
+    dfs = []
+    for df in tqdm(pool.imap_unordered(df_from_gzipped_json_lines, glob_or_list),
+                   total=len(glob_or_list)):
+        dfs.append(df)
+
+    pool.close()
+    pool.join()
+    return pd.concat(dfs)
 
 
 class CreateDataFrame:
@@ -24,7 +60,7 @@ class CreateDataFrame:
         :return:
         """
         self.config = config
-        self.df = _df_from_flattened_json(config)
+        self.df = bulk_gzipped_jsonline_files_to_df(config['filenames'])
 
     def create_msa_data_frame(self):
         """
