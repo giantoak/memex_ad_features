340e5595d543ab0b3efcece71bea6c5bae7fed7b
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-08-01 00:56:32 -0400

    Killed bad line

--- lattice_json_tools/__init__.py
+++ lattice_json_tools/__init__.py
@@ -3,4 +3,3 @@ from .flatten_json import stream_lattice_jsonline_gzip_file_to_file
 
 from .json_to_pandas import gzipped_jsonline_file_to_df
 from .json_to_pandas import bulk_gzipped_jsonline_files_to_dfs
-s
