f1ff8d297523881b066433ecd3977cbb94ac8be0
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-08-02 11:31:28 -0400

    Aggressively omit columns (hack memory fix)

--- lattice_json_tools/json_to_pandas.py
+++ lattice_json_tools/json_to_pandas.py
@@ -10,26 +10,35 @@ def gzipped_jsonline_file_to_df(fpath):
 
     cols_to_use = ['_id', 'age',
                    'email', 'flag',
-                   'location_city_lat_lon', 'location_city_name',
-                   'location_state_lat_lon', 'location_state_name',
+                   # 'location_city_lat_lon', 'location_state_lat_lon',
+                   'location_city_name', 'location_state_name',
                    'phone', 'postdatetime',
                    'rate']
 
     df = DataFrame.from_records((json.loads(line) for line in gzip.open(fpath)),
                                 columns=cols_to_use)
 
-    for loc_type in ['city', 'state']:
-        col = 'location_{}_lat_lon'.format(loc_type)
-        df.loc[:, col] = df.loc[:, col].apply(lambda x:
-                                              (x[0], x[1]) if isinstance(x, list)
-                                              else (nan, nan))
+    # For the moment, we're skipping ingesting lat_lons to save memory
+    df = df.rename(columns={'location_city_name': 'city',
+                            'location_state_name': 'state'})
+                              # 'location_city_lat_lon': 'city_lat_lon',
+                              # 'location_state_lat_lon': 'state_lat_lon'})
+
+    # For the moment, we're skipping ingesting lat_lons to save memory
+    # for loc_type in ['city', 'state']:
+    #     col = '{}_lat_lon'.format(loc_type)
+    #     df.loc[:, col] = df.loc[:, col].apply(lambda x:
+    #                                           (x[0], x[1]) if isinstance(x, list)
+    #                                           else (nan, nan))
 
     df.age = df.age.astype(float)
-    return df.rename(columns={'location_city_name': 'city',
-                              'location_city_lat_lon': 'city_lat_lon',
-                              'location_state_name': 'state',
-                              'location_state_lat_lon': 'state_lat_lon'}
-                     ).drop_duplicates()
+
+    # For the moment, we are dropping all entities with no age or rate
+    # to save memory
+    df = df.dropna(how='all',
+                   subset=['age', 'rate'])
+
+    return df.drop_duplicates()
 
 
 def bulk_gzipped_jsonline_files_to_dfs(glob_or_list, nproc=10, merge_threshold=(5, 10)):
