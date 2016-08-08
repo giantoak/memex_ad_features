a99e333babb6ff17dcca484027b3160c773cf620
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-08-08 16:38:29 -0700

    Write results to data/output/ not data/

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ def run_location_features(dfm):
     make_msa = MakeMSA(df).get_msa_features()
     for loc_type in ['city', 'state']:
         make_msa['{}_stats'.format(loc_type)].to_csv(
-            'data/location_characteristics_{}.csv'.format(loc_type),
+            'data/output/location_characteristics_{}.csv'.format(loc_type),
             sep='\t',
             encoding='utf8')
 
@@ -29,12 +29,12 @@ def run_ad_features(dfm):
     df = dfm.create_ad_dataframe()
 
     # Get the city and state features
-    city_features = pd.read_table('data/location_characteristics_city.csv')
-    state_features = pd.read_table('data/location_characteristics_state.csv')
+    city_features = pd.read_table('data/output/location_characteristics_city.csv')
+    state_features = pd.read_table('data/output/location_characteristics_state.csv')
 
     make_ad = MakeAd(city_features, state_features, df)
     ad_features = make_ad.get_ad_features()
-    ad_features.to_csv('data/ad_characteristics.csv',
+    ad_features.to_csv('data/output/ad_characteristics.csv',
                        sep='\t',
                        encoding='utf8',
                        index=False)
@@ -48,7 +48,7 @@ def run_entity_features(dfm, entity):
     """
     df = dfm.create_entity_dataframe(entity)
     make_entity = MakeEntity(df, entity).get_entity_features()
-    make_entity.to_csv('data/{}_characteristics.csv'.format(entity),
+    make_entity.to_csv('data/output/{}_characteristics.csv'.format(entity),
                        sep='\t',
                        encoding='utf8',
                        index=False)
