cb96aad4c621dc49c85608607e08c3da2c9f67eb
Gabriel <gstub728@gmail.com>
2016-08-29 13:46:23 -0400

    run.py - added in functionality to create directory with date so results can be saved there.

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from make_msa import MakeMSA
 from make_ad import MakeAd
 from make_entity import MakeEntity
 import pandas as pd
+import os
+import datetime
 from glob import glob
 
 #  config = {'filenames': glob.glob('/home/ubuntu/2016_summer_camp/classifier/data/initial/lattice/*gz')}
@@ -10,7 +12,7 @@ from glob import glob
 config = {'filenames': glob('/home/ubuntu/flat_data/*.json.gz')}
 
 
-def run_location_features(dfm):
+def run_location_features(dfm, directory):
     """
     Will get features by city and state
     :param create_dataframe.DFManager dfm:
@@ -19,12 +21,12 @@ def run_location_features(dfm):
     make_msa = MakeMSA(df).get_msa_features()
     for loc_type in ['city', 'state']:
         make_msa['{}_stats'.format(loc_type)].to_csv(
-            '/home/ubuntu/memex_ad_features/data/location_characteristics_{}.csv'.format(loc_type),
+            '/home/ubuntu/memex_ad_features/data/{0}/location_characteristics_{1}.csv'.format(directory, loc_type),
             sep='\t',
             encoding='utf8')
 
 
-def run_ad_features(dfm):
+def run_ad_features(dfm, directory):
     """
     Will run the ad features
     :param create_dataframe.DFManager dfm:
@@ -32,18 +34,18 @@ def run_ad_features(dfm):
     df = dfm.create_ad_dataframe()
 
     # Get the city and state features
-    city_features = pd.read_table('/home/ubuntu/memex_ad_features/data/location_characteristics_city.csv')
-    state_features = pd.read_table('/home/ubuntu/memex_ad_features/data/location_characteristics_state.csv')
+    city_features = pd.read_table('/home/ubuntu/memex_ad_features/data/{0}/location_characteristics_city.csv'.format(directory))
+    state_features = pd.read_table('/home/ubuntu/memex_ad_features/data/{0}/location_characteristics_state.csv'.format(directory))
 
     make_ad = MakeAd(city_features, state_features, df)
     ad_features = make_ad.get_ad_features()
-    ad_features.to_csv('/home/ubuntu/memex_ad_features/data/ad_characteristics.csv',
+    ad_features.to_csv('/home/ubuntu/memex_ad_features/data/{0}/ad_characteristics.csv'.format(directory),
                        sep='\t',
                        encoding='utf8',
                        index=False)
 
 
-def run_entity_features(dfm, entity):
+def run_entity_features(dfm, entity, directory):
     """
 
     :param create_dataframe.DFManager dfm:
@@ -51,13 +53,17 @@ def run_entity_features(dfm, entity):
     """
     df = dfm.create_entity_dataframe(entity)
     make_entity = MakeEntity(df, entity).get_entity_features()
-    make_entity.to_csv('/home/ubuntu/memex_ad_features/data/{}_characteristics.csv'.format(entity),
+    make_entity.to_csv('/home/ubuntu/memex_ad_features/data/{0}/{1}_characteristics.csv'.format(directory, entity),
                        sep='\t',
                        encoding='utf8',
                        index=False)
 
 
 def main():
+
+    # First create the directory that we will save everything to
+    mydir = os.path.join('home/ubuntu/memex_ad_features/data/', datetime.datetime.now().strftime('%Y%m%d'))
+    os.makedirs(mydir)
 
     print('Initializing DF Manager...')
     dfm = DFManager(config)
