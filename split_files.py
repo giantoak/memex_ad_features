08616c2fc07a4577e9902fdd8e2a7e1b244ac3b8
Gabriel <gstub728@gmail.com>
2016-11-21 10:48:24 -0500

    split files

--- split_files.py
+++ split_files.py
@@ -6,17 +6,18 @@ from multiprocessing import Lock, Pool
 
 def split_file(filename):
     count = 0
-    outfile = gzip.open('{0}{1}_{2}'.format(config['split_file_directory'], os.path.basename(filename), count), 'wb')
+    outfile = gzip.open('{0}{1}_{2}.gz'.format(config['split_file_directory'], os.path.basename(filename), count), 'wb')
     for line in gzip.open(filename):
         if count % 100000 == 0:
             print '{0} lines read from {1}'.format(count, filename)
         if count % 500000 == 0:
             outfile.close()
-            outfile = gzip.open('{0}{1}_{2}'.format(config['split_file_directory'], os.path.basename(filename), count), 'wb')
+            outfile = gzip.open('{0}{1}_{2}.gz'.format(config['split_file_directory'], os.path.basename(filename), count), 'wb')
         outfile.write(line)
         count += 1
 
     outfile.close()
+    print '****** Finished {0}'.format(filename)
 
 if __name__ == '__main__':
     config = Parser().parse_config('config/config.conf', 'AWS')
