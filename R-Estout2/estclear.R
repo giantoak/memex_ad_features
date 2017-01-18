429112185ac804b4f7f793cdf60780dbcb7735b5
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2017-01-18 14:23:53 -0800

    Remove old processing files

--- R-Estout2/estclear.R
+++
@@ -1,6 +0,0 @@
-`estclear` <-
-function(tableName='ccl'){
-    assign(x=tableName,value=NULL,envir=globalenv())
-    return(1)
-}
-
--- make_lemas.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-lemas = pd.read_csv('ucr_lemas_msa.csv')
-lemas = lemas[['msa_parents', 'swnauthemp', 'swnftemp']]
-lemas.rename(columns={'msa_parents': 'census_msa_code'}, inplace=True)
-aggregated = lemas.groupby(['census_msa_code'])['swnauthemp', 'swnftemp'].sum()
-aggregated.to_csv('lemas.csv')
